4b8a68c422fa7f095fb65811756a7e97ae770289
torarinvik <torarinvik@gmail.com>
2026-09-04 17:42:46 +0200

    fix: enforce profiler ABI integer bounds

--- test/profile_protocol_smoke.py
+++ test/profile_protocol_smoke.py
@@ -45,6 +45,7 @@ def main() -> int:
     assert_unsupported_version(profiler, "ELISA_PROFILE\t2\tmeta\t0\t0\t0")
     assert_malformed(profiler, "ELISA_PROFILE\t1\tmeta\tnot-a-number\t0\t0")
     assert_malformed(profiler, "ELISA_PROFILE\t1\tmeta\t-1\t0\t0")
+    assert_malformed(profiler, "ELISA_PROFILE\t1\tmeta\t18446744073709551616\t0\t0")
     assert_malformed(
         profiler,
         "ELISA_PROFILE\t1\tlocation\t1\tmain\tnot-a-line\t1\t-\t0\t0\t0\t0\t0\t0\t0",
@@ -52,6 +53,10 @@ def main() -> int:
     assert_malformed(
         profiler,
         "ELISA_PROFILE\t1\tlocation\t3\tmain\t1\t1\t-\t0\t-1\t0\t0\t0\t0\t0",
+    )
+    assert_malformed(
+        profiler,
+        "ELISA_PROFILE\t1\tlocation\t2\tmain\t1\t1\tvalue\t1\t9223372036854775808\t0\t0\t0\t0\t0",
     )
     empty = profiler.parse_profile("target wrote no collector records\n", allow_missing=True)
     assert empty[0]["events"] == 0
