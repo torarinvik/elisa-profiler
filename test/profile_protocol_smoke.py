6ca3a5b4a79d6e09a510951c133ad83be5e80b38
torarinvik <torarinvik@gmail.com>
2026-09-04 17:28:13 +0200

    fix: validate profiler protocol counters

--- test/profile_protocol_smoke.py
+++ test/profile_protocol_smoke.py
@@ -44,9 +44,14 @@ def main() -> int:
     profiler = load_profiler()
     assert_unsupported_version(profiler, "ELISA_PROFILE\t2\tmeta\t0\t0\t0")
     assert_malformed(profiler, "ELISA_PROFILE\t1\tmeta\tnot-a-number\t0\t0")
+    assert_malformed(profiler, "ELISA_PROFILE\t1\tmeta\t-1\t0\t0")
     assert_malformed(
         profiler,
         "ELISA_PROFILE\t1\tlocation\t1\tmain\tnot-a-line\t1\t-\t0\t0\t0\t0\t0\t0\t0",
+    )
+    assert_malformed(
+        profiler,
+        "ELISA_PROFILE\t1\tlocation\t3\tmain\t1\t1\t-\t0\t-1\t0\t0\t0\t0\t0",
     )
     empty = profiler.parse_profile("target wrote no collector records\n", allow_missing=True)
     assert empty[0]["events"] == 0
